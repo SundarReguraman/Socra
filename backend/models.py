3eaa4fc4a229983b6f4e73337faebd7842c49966
reguramansundar@gmail.com <reguramansundar@gmail.com>
2026-06-29 12:03:07 +0530

    feat: add SQLAlchemy database models

---
+++ backend/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, String, DateTime, Integer, ForeignKey, Text
+from datetime import datetime, timezone
+from database import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid = True), primary_key = True, default= uuid.uuid4)
+    name = Column(String, nullable = False)
+    email = Column(String, nullable = False)
+    auth_provider = Column(String, nullable = False)
+    signup_time = Column(DateTime, nullable= False, default = lambda: datetime.now(timezone.utc))
+
+class Session(Base):
+    __tablename__ = "sessions"
+
+    id = Column(UUID(as_uuid = True), primary_key = True, default = uuid.uuid4)
+    user_id = Column(UUID(as_uuid = True),ForeignKey("users.id"), nullable = False)
+    problem_text = Column(Text, nullable = False)
+    current_hint_level = Column(Integer, nullable = False, default = 1)
+    status = Column(String, nullable = False, default = "active")
+    created_at = Column(DateTime, nullable = False, default = lambda: datetime.now(timezone.utc))
+
+class Messages(Base):
+    __tablename__ = "messages"
+
+    id = Column(UUID(as_uuid = True), primary_key= True, default= uuid.uuid4)
+    session_id = Column(UUID(as_uuid = True), ForeignKey("sessions.id"), nullable = False)
+    sender = Column(String, nullable = False)
+    content = Column(Text, nullable = False)
+    progress_score = Column(Integer, nullable = True)
+    hint_level_at_time = Column(Integer, nullable = True)
+    created_at = Column(DateTime, nullable = False, default = lambda: datetime.now(timezone.utc))
+    
+
+
+
+
