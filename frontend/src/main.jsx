3110fcafa292f5cd3f74669d3ead5f5dc53fff07
reguramansundar@gmail.com <reguramansundar@gmail.com>
2026-07-05 00:50:39 +0530

    feat: add home.jsx, main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowseRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Session from './pages/Session'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/" elements = {<Home />} />
+      <Route path = "/session/:id" element = {<Session />} />
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
+
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { createSession } from "../services/api"
+
+function Home() {
+    const [problemText, setProblemText] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    async function handleStart() {
+        if(!problemText.trim()) return 
+
+        setLoading(true)
+        setError(null)
+
+        try {
+            const session = await createSession(problemText)
+            navigate('/session/${session.id}', {
+                state:{
+                    firstQuestion: session.content,
+                    hintLevel: session.hint_level
+                }
+            })
+        } catch (err) {
+            setError("Something went wrong. Please try again.")
+
+        } finally{
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className = "home">
+            <h1>Socra</h1>
+            <p>The insight isn't handed to you. It's discovered - one question at a time.</p>
+
+            <textarea
+                placeholder = "Paste your Leetcode problem here..."
+                value = {problemText}
+                onChange={(e) => setProblemText(e.target.value)}
+                rows = {10}
+            />
+
+            {error && <p className = "error">{error}</p>}
+
+            <button onClick = {handleStart} disabled = {loading}>
+                {loading ? "Starting..." : "Start Session"}
+            </button>
+
+        </div>
+    )
+}
+export default Home
